faa13557b166ffd565d421b43c1dc7937326ac03
Viktor Legusov <viktring@ukr.net>
2024-12-02 13:09:58 +0200

    null

---
+++ js/JS task-3.js
@@ -0,0 +1,12 @@
+function getElementWidth(content, padding, border) {
+    const c = parseFloat(content);
+    const p = parseFloat(padding);
+    const b = parseFloat(border);
+    const border-box = ;
+}
+getElementWidth("50px", "8px", "4px");
+getElementWidth("60px", "12px", "8.5px");
+getElementWidth("200px", "0px", "0px");
+console.log(getElementWidth("50px", "8px", "4px")); // 74
+console.log(getElementWidth("60px", "12px", "8.5px")); // 101
+console.log(getElementWidth("200px", "0px", "0px")); // 200
